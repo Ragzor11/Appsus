8be57352caf45cd8a0775226229c7e01d0cd524d
DenisBerezin <compA321@gmail.com>
2023-09-06 12:51:41 +0300

    setting up the mail service

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,38 @@
 // mail service
+
+import { utilService } from '../../../services/util.service'
+import { storageService } from '../../../services/storage.service'
+
+
+const MAIL_KEY = 'mailDB'
+
+const loggedInUser = {
+    email: 'user@appsus.com',
+    fullname: 'Mahatma Appsus'
+}
+_createMails()
+
+
+function _createMail(subject, body, from, to){
+    const mail={
+    id: utilService.makeId ,
+    subject,
+    body,
+    isRead: false,
+    sentAt: new Date(),
+    removedAt: null,
+    from,
+    to,
+    }
+    return mail
+}
+function _createMails(){
+    let mails = storageService.loadFromStorage(MAIL_KEY)
+    if (!mails || !mails.length){
+        mails = []
+        mails.push(_createMail('start',utilService.makeLorem,'test@test.com','user@appsus.com'))
+        mails.push(_createMail('middle',utilService.makeLorem,'test@test.com','user@appsus.com'))
+        mails.push(_createMail('end',utilService.makeLorem,'test@test.com','user@appsus.com'))
+        storageService.saveToStorage(MAIL_KEY,mails)
+    }
+}
